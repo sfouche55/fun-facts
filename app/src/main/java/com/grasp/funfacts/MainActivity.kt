8c0f10ed19ec6f101c28915275071ff007096779
Senta Fouché <64000466+sfouche55@users.noreply.github.com>
2021-04-15 14:41:38 +0200

    Remove unnecessary statements.

--- app/src/main/java/com/grasp/funfacts/MainActivity.kt
+++ app/src/main/java/com/grasp/funfacts/MainActivity.kt
@@ -3,19 +3,14 @@ package com.grasp.funfacts
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.RelativeLayout
 import android.widget.TextView
 
-//import android.widget.Toast
-
 class MainActivity : AppCompatActivity() {
-    private val tag = MainActivity::class.simpleName
     private val factBook = FactBook()
     private val colorWheel = ColorWheel()
 
-    // View vars
     private var factTextView: TextView? = null
     private var showFactButton: Button? = null
     private var relativeLayout: RelativeLayout? = null
@@ -24,7 +19,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Assign the Views from the layout file to the corresponding variables
         factTextView = findViewById(R.id.factTextView)
         showFactButton = findViewById(R.id.showFactButton)
         relativeLayout = findViewById(R.id.relativeLayout)
@@ -34,12 +28,8 @@ class MainActivity : AppCompatActivity() {
         showFactButton!!.setOnClickListener {
             setRandomFact(factTextView, showFactButton, relativeLayout)
         }
-
-        //Toast.makeText(this, "Activity created successfully!", Toast.LENGTH_LONG).show()
-        //Log.d(tag, "Logging from the onCreate() method.")
     }
 
-    // Update the fact TextView with a randomly selected fact
     private fun setRandomFact(view: TextView?, button: Button?, layout: RelativeLayout?) {
         val fact = factBook.getFact()
         val color = colorWheel.getColor()
